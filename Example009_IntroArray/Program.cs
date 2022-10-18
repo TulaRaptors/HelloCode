177d6000c7bdec13aeb9fb9bd3fc6269ff6c7c5f
Sergey_Podgornov <psv_23@mail.ru>
2022-10-19 00:33:59 +0300

    Введение в массивы

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int[] array = {11,21,31,41,15,61,17,18,19};
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+} 
